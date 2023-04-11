ca134cc0d3876856b7f542b25c8703ef4ffab76d
hungmnpc <hungkaiken2@gmail.com>
2023-04-11 14:32:48 +0700

    first commit

---
+++ src/main/java/com/facenet/shipsregistry/ShipsregistryApplication.java
@@ -0,0 +1,12 @@
+package com.facenet.shipsregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipsregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipsregistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/facenet/shipsregistry/ShipsregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facenet.shipsregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipsregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
